ef97d095189a840e95ff51c626baaec14644873e
高一航 <dustookk@gmail.com>
2013-09-17 18:32:45 +0800

    Keep Number Info picked from the contact app

---
+++ Findme2/src/com/gyh/findme2/model/NumberInfo.java
@@ -0,0 +1,52 @@
+package com.gyh.findme2.model;
+
+import com.gyh.findme2.R;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+
+public class NumberInfo {
+	private String number;
+	private String type;
+
+	public NumberInfo(Context context, String numberStr, int itype) {
+		setNumber(numberStr);
+		setType(context, itype);
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setNumber(String numberStr) {
+		this.number = numberStr.replaceAll("/[^0-9]/", "");
+	}
+
+	public void setType(Context context, int itype) {
+		switch (itype) {
+		case ContactsContract.CommonDataKinds.Phone.TYPE_MAIN:
+			this.type = context.getString(R.string.type_main);
+			break;
+		case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+			this.type = context.getString(R.string.type_home);
+			break;
+		case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+			this.type = context.getString(R.string.type_mobile);
+			break;
+		case ContactsContract.CommonDataKinds.Phone.TYPE_OTHER:
+			this.type = context.getString(R.string.type_other);
+			break;
+		case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+			this.type = context.getString(R.string.type_work);
+			break;
+		case ContactsContract.CommonDataKinds.Phone.TYPE_WORK_MOBILE:
+			this.type = context.getString(R.string.type_work_mobile);
+			break;
+		}
+
+	}
+}
