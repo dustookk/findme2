2689881a08e3e986205d2fd2f885233cfae5c998
高一航 <dustookk@gmail.com>
2013-09-17 18:34:18 +0800

    add v w ...

--- Findme2/src/com/gyh/findme2/util/LogUtil.java
+++ Findme2/src/com/gyh/findme2/util/LogUtil.java
@@ -18,6 +18,29 @@ public class LogUtil {
 	}
 	
 	public static void e(Throwable tr) {
-		Log.d(TAG,"", tr);
+		Log.e(TAG,"", tr);
 	}
+	public static void i(String msg) {
+		Log.i(TAG, msg);
+	}
+	
+	public static void i(Throwable tr) {
+		Log.i(TAG,"", tr);
+	}
+	
+	public static void v(String msg) {
+		Log.v(TAG, msg);
+	}
+	
+	public static void v(Throwable tr) {
+		Log.v(TAG,"", tr);
+	}
+	public static void w(String msg) {
+		Log.w(TAG, msg);
+	}
+	
+	public static void w(Throwable tr) {
+		Log.w(TAG,"", tr);
+	}
+	
 }
