792cc4e99c012f75972976b84b1585e10832d7a8
高一航 <dustookk@gmail.com>
2013-09-23 17:36:46 +0800

    now we can control the service switch button

--- Findme2/src/com/gyh/findme2/ui/SlidingViewHolder.java
+++ Findme2/src/com/gyh/findme2/ui/SlidingViewHolder.java
@@ -1,6 +1,11 @@
 package com.gyh.findme2.ui;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -9,17 +14,16 @@ import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 import com.gyh.findme2.R;
-import com.gyh.findme2.util.LogUtil;
+import com.gyh.findme2.service.FindmeService;
 
-public class SlidingViewHolder implements OnClickListener {
+public class SlidingViewHolder implements OnClickListener,IViewHolder {
 	private Activity mActivity;
 	private ImageView switchView;
-	private boolean isServiceOn = false;
 	
 	public SlidingViewHolder(Activity activity) {
 		this.mActivity = activity;
 	}
-	
+	@Override
 	public View getView() {
 		LayoutInflater layoutInflater = (LayoutInflater)mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View slidingView = layoutInflater.inflate(R.layout.layout_sliding, null);
@@ -32,11 +36,9 @@ public class SlidingViewHolder implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		LogUtil.d("click");
 		switch (v.getId()) {
 		case R.id.rl_switch:
-			//TODO
-			changePic();
+			switchServiceState();
 			break;
 		case R.id.rl_setting:
 			mActivity.startActivity(new Intent(mActivity,SettingActivity.class));
@@ -49,12 +51,58 @@ public class SlidingViewHolder implements OnClickListener {
 		}
 	}
 
-	private void changePic() {
-		if(isServiceOn) {
+	private void switchServiceState() {
+		Intent serviceIntent = new Intent(mActivity, FindmeService.class);
+		boolean isServiceOn = checkIfServiceIsStarted();
+		if(isServiceOn) { // service was on
 			switchView.setImageResource(R.drawable.switch_off);
-		}else {
+			mActivity.stopService(serviceIntent);
+		}else { //service was off
 			switchView.setImageResource(R.drawable.switch_on);
+			mActivity.startService(serviceIntent);
 		}
-		isServiceOn = !isServiceOn;
 	}
+	
+	private boolean checkIfServiceIsStarted() {
+		ActivityManager activityManager = (ActivityManager) mActivity
+				.getSystemService(Context.ACTIVITY_SERVICE);
+		List<RunningServiceInfo> runningServiceList = activityManager
+				.getRunningServices(Integer.MAX_VALUE);
+		for (RunningServiceInfo runningServiceInfo : runningServiceList) {
+			ComponentName service = runningServiceInfo.service;
+			String className = service.getClassName();
+			if ("com.gyh.findme2.service.FindmeService".equals(className)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void onActivityStart() {
+		boolean isServiceOn = checkIfServiceIsStarted();
+		if(isServiceOn) { // service is on
+			switchView.setImageResource(R.drawable.switch_on);
+		}else { //service is off
+			switchView.setImageResource(R.drawable.switch_off);
+		}
+	}
+
+	@Override
+	public void onActivityStop() {
+		
+	}
+	@Override
+	public void onActivityResume() {
+		
+	}
+	@Override
+	public void onActivityPause() {
+		
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		
+	}
+	
 }
